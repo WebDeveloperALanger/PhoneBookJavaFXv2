5a04a4e9162b8b3c7b9751ffc2a8e18d224752a5
Andre Langer <aldev@langer.guru>
2023-05-30 11:33:54 +0200

    init scene layout

--- src/main/java/guru/langer/phonebookjavafxv2/controller/PhoneBookController.java
+++ src/main/java/guru/langer/phonebookjavafxv2/controller/PhoneBookController.java
@@ -1,14 +1,37 @@
 package guru.langer.phonebookjavafxv2.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class PhoneBookController {
+
+    @FXML
+    public TextField fieldName;
+    @FXML
+    public TextField fieldNumber;
+    @FXML
+    public TextField fieldMail;
+    @FXML
+    public TableColumn colName;
+    @FXML
+    public TableColumn colNumber;
+    @FXML
+    public TableColumn colMail;
+    @FXML
+    public Label labelInfo;
+    @FXML
+    public TableView tableMain;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onSave(ActionEvent actionEvent) {
+    }
+
+    void initialize() {
+        System.out.println("init PhoneBookController");
     }
 }
